f55f5e7921e34952369b822971042caa8ebef910
AmirHossein Mirzaei <amir13881188@gmail.com>
2026-09-14 10:02:19 +0330

    login with code updated

--- 1.py
+++ 1.py
@@ -1,6 +1,8 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 driver=webdriver.ChromiumEdge()
 driver.get("https://typekadeh.com")
 btn=driver.find_element(By.CLASS_NAME,"close-button")
