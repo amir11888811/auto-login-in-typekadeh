b4a6fa456f6c78abc970100d17c98ed695f018e7
AmirHossein Mirzaei <amir13881188@gmail.com>
2026-09-14 10:03:12 +0330

    login with code updated

--- 1.py
+++ 1.py
@@ -3,3 +3,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+def slow_type(element, text, delay=0.1):
+    element.clear()
+    for char in text:
+        element.send_keys(char)
+        time.sleep(delay)
