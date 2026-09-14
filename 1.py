4e2128884e4a48178f423ba5c82f5fa5b4fa7dda
AmirHossein Mirzaei <amir13881188@gmail.com>
2026-09-14 10:04:08 +0330

    login with code updated

--- 1.py
+++ 1.py
@@ -18,3 +18,7 @@ wait = WebDriverWait(driver, 10)
 
 driver.get("https://typekadeh.com")
 time.sleep(2)
+
+btn1 = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "header button.tk-btn")))
+driver.execute_script("arguments[0].click();", btn1)
+time.sleep(1.5)
